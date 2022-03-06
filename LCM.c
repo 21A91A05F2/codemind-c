5eae5130cef5d0d82e6507b3a7289eb7c979c39f
sireesha kudupuri <97424637+21A91A05F2@users.noreply.github.com>
2022-03-06 17:18:42 +0530

    #include<stdio.h> int main() {     int a,b,min;     scanf("%d %d",&a,&b);     if(a>b)         min=a;     else         min=b;     while(1)     {         if(min%a==0 && min%b==0)         {             printf("%d",min);             break;         }         ++min;     }
    }

---
+++ LCM.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,min;
+    scanf("%d %d",&a,&b);
+    if(a>b)
+        min=a;
+    else
+        min=b;
+    while(1)
+    {
+        if(min%a==0 && min%b==0)
+        {
+            printf("%d",min);
+            break;
+        }
+        ++min;
+    }
+    
+}
