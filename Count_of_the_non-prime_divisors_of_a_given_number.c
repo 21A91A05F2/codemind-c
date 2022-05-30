b4d8296bcd31372a1e977d3cb8c38464eb2e4906
sireesha <97424637+21A91A05F2@users.noreply.github.com>
2022-05-30 06:17:44 +0530

    #include<stdio.h> int main() {     int n,i,j,c=0,co=0;     scanf("%d",&n);//8     for(i=1;i<=n;i++)//2<=8     {         if(n%i==0)//8%2==0         {             for(j=1;j<=i;j++)//j=2<=2             {                 if(i%j==0)//2%2==0                 {                     c++;//c=2                 }             }             if(c!=2)             {                 co++;//co=1             }             c=0;         }     }     printf("%d",co); }

--- Count_of_the_non-prime_divisors_of_a_given_number.c
+++ Count_of_the_non-prime_divisors_of_a_given_number.c
@@ -1,22 +1,22 @@
 #include<stdio.h>
 int main()
 {
-    int n,i,c=0,j,co=0;
-    scanf("%d",&n);
-    for(i=1;i<=n;i++)
+    int n,i,j,c=0,co=0;
+    scanf("%d",&n);//8
+    for(i=1;i<=n;i++)//2<=8
     {
-        if(n%i==0)
+        if(n%i==0)//8%2==0
         {
-            for(j=1;j<=i;j++)
+            for(j=1;j<=i;j++)//j=2<=2
             {
-                if(i%j==0)
+                if(i%j==0)//2%2==0
                 {
-                    c++;
+                    c++;//c=2
                 }
             }
             if(c!=2)
             {
-                co++;
+                co++;//co=1
             }
             c=0;
         }
