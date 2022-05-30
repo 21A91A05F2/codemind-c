3e5dace7e8a7bc12bf67408f163974eaf3a592b9
sireesha <97424637+21A91A05F2@users.noreply.github.com>
2022-05-30 06:31:11 +0530

    #include<stdio.h> int main() {     int n,arr[100],i,j,k,c=0,d=0,e=0;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     for(i=0;i<n;i++)     {         if(arr[i]%2!=0)         {             c=i;             break;         }     }     for(j=n-1;j>=0;j--)     {         if(arr[j]%2!=0)         {             d=j;             break;         }     }     for(k=c+1;k<d;k++)     {         if(arr[k]%2==0)         {             e++;         }     }     printf("%d",e); }

---
+++ count_even_numbers_between_odd_numbers.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+int main()
+{
+    int n,arr[100],i,j,k,c=0,d=0,e=0;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        if(arr[i]%2!=0)
+        {
+            c=i;
+            break;
+        }
+    }
+    for(j=n-1;j>=0;j--)
+    {
+        if(arr[j]%2!=0)
+        {
+            d=j;
+            break;
+        }
+    }
+    for(k=c+1;k<d;k++)
+    {
+        if(arr[k]%2==0)
+        {
+            e++;
+        }
+    }
+    printf("%d",e);
+}
