6ea02620d66cca7a0f3800dfd2b796b3ac561c45
sireesha <97424637+21A91A05F2@users.noreply.github.com>
2022-07-01 21:10:59 +0530

    #include<stdio.h> int main() {     int n,i,arr[i],res;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     for(i=0;i<n;i++)     {         if(arr[i]%2!=0)         res=arr[i];     }     printf("%d",res);
    }

---
+++ last_odd_number_in_an_array.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    int n,i,arr[i],res;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        if(arr[i]%2!=0)
+        res=arr[i];
+    }
+    printf("%d",res);
+    
+}
